14a40b20cac19ab44b99902ababef87abfeb8fb4
dsemkin <semkin_dmitriy10@vk.com>
2025-11-28 19:11:11 +0400

    Cleanup code

--- src/test/java/second/ArrayGeneratorTypeTest.java
+++ src/test/java/second/ArrayGeneratorTypeTest.java
@@ -3,7 +3,7 @@ package second;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 class ArrayGeneratorTypeTest {
     @Test
